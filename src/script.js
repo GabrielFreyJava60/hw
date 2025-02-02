62298797a194736d5828c7554add33cd713719d6
Gabriel <filinjd@gmail.com>
2025-02-02 18:59:51 +0200

    1

---
+++ src/script.js
@@ -0,0 +1,35 @@
+const detailedImages = document.querySelector(".dImage");
+const detailedTitle = document.querySelector(".detailed-title");
+const boxesElements = document.querySelectorAll(".gallery--item");
+
+function setDetails(boxElement) {
+  const imageElement = boxElement.querySelector(".gallery--item_image");
+  const titleElement = boxElement.querySelector(".gallery--item_title");
+
+  if (imageElement) {
+    detailedImages.src = imageElement.src;
+    detailedImages.classList.remove("animation-image");
+
+    setTimeout(function () {
+      detailedImages.classList.add("animation-image"); 
+    }, 10); 
+  }
+
+  if (titleElement) {
+    detailedTitle.innerHTML = titleElement.dataset.detailedText;
+  }
+
+  detailedTitle.classList.remove("animation-text");
+
+  setTimeout(function () {
+    detailedTitle.classList.add("animation-text");
+  }, 10);
+}
+
+
+for (let i = 0; i < boxesElements.length; i++) {
+  boxesElements[i].addEventListener("click", function () {
+    setDetails(boxesElements[i]);
+  });
+}
+
